69f3f6eeefe1208b697835dd592e01db9e9de8eb
Zack Wins <el_hefe@mbp.attlocal.net>
2023-08-25 18:06:44 -0400

    removed terinary

--- pages/api/publish.js
+++ pages/api/publish.js
@@ -15,7 +15,7 @@ const handleInscribing = async (fileAsBase64, mimeType, receiverAddress, metadat
       const minimumSatoshis = Math.ceil(fileAsBase64.length / 5) + 5;
       console.log({ minimumSatoshis })
       const paymentPk = PrivateKey.from_wif(process.env.NEWS_MINT_KEY);
-      let signKey = PrivateKey.from_wif(signerKey ? signerKey.toString() : process.env.NEWS_SIGN_KEY);
+      let signKey = PrivateKey.from_wif(process.env.NEWS_SIGN_KEY);
       const changeAddress =  process.env.NEWS_CHANGE_ADDRESS//process.env.HTML_ART_MINT_CHANGE_ADDRESS;
       let utxoResponse = await getUnspentTransactions(minimumSatoshis, changeAddress);
       console.log("Signer Key", signKey);
