fef7aabadce92bf217945120b4a5482937447231
Zack Wins <el_hefe@mbp.attlocal.net>
2023-08-28 00:30:42 -0400

    updated local search a to link

--- pages/index.js
+++ pages/index.js
@@ -47,10 +47,10 @@ export default function Home() {
             <p className="m-0 max-w-[30ch] text-sm opacity-50">View Posts from OrdPost.com</p>
           </Link>
 
-          <a href="/search" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <Link href="/search" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
             <h2 className="mb-3 text-2xl font-semibold">Search</h2>
             <p className="m-0 max-w-[30ch] text-sm opacity-50">Search the world of posts. </p>
-          </a>
+          </Link>
 
           <a href="https://github.com/ZacharyWeiner/ord-post" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
             <h2 className="mb-3 text-2xl font-semibold">Clone</h2>
