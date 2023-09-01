e54737d52a8de898499a7393f915fb78d775257d
Zack Wins <el_hefe@mbp.attlocal.net>
2023-08-31 21:26:08 -0400

    added a link from the homepage to the profile page

--- pages/index.js
+++ pages/index.js
@@ -58,10 +58,10 @@ export default function Home() {
             <p className="m-0 max-w-[30ch] text-sm opacity-50">Search the world of posts. </p>
           </Link>
 
-          <a href="https://github.com/ZacharyWeiner/ord-post" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-            <h2 className="mb-3 text-2xl font-semibold">Clone</h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">Clone this project to run on your own</p>
-          </a>
+          <Link href="/profile" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <h2 className="mb-3 text-2xl font-semibold">Profile</h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">View & Export Your Keys </p>
+          </Link>
         </section>
         
       </main>
